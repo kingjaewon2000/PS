e60f972eb3984c4ef689ac1495ef0d6d6a2c9664
kingjaewon2000 <50149671+kingjaewon2000@users.noreply.github.com>
2020-12-22 14:55:12 +0900

    Add files via upload

---
+++ 2020-12/미래도시.py
@@ -0,0 +1,30 @@
+INF = int(1e9)
+
+N, M = map(int, input().split())
+graph = [[INF] * (N + 1) for _ in range(N + 1)]
+min_dist = 0
+
+for _ in range(M):
+    a, b = map(int, input().split())
+
+    graph[a][b] = 1
+    graph[b][a] = 1
+
+X, K = map(int, input().split())
+
+for a in range(1, N + 1):
+    for b in range(1, N + 1):
+        if a == b:
+            graph[a][b] = 0
+
+for k in range(1, N + 1):
+    for a in range(1, N + 1):
+        for b in range(1, N + 1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+
+min_dist = graph[1][K] + graph[K][X]
+
+if min_dist < INF:
+    print(min_dist)
+else:
+    print(-1)
